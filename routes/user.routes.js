d07c67d44e8c9630db54504198b260bb7032947e
patryk <paniczgg@gmail.com>
2025-05-14 16:56:48 +0200

    added authorize

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,25 +1,25 @@
 import {Router} from "express"
 
+import authorize from '../middlewares/auth.middleware.js'
+
+import {getUser, getUsers} from "../controllers/user.controller.js";
+
 const userRouter = Router()
 
-userRouter.get("/", (req, res) => {
-  res.send({ title: "GET all users" })
-});
+userRouter.get("/", getUsers);
 
-userRouter.get("/:id", (req, res) => {
-  res.send({ title: "GET user details" })
-});
+userRouter.get("/:id", authorize, getUser)
 
 userRouter.post("/", (req, res) => {
-  res.send({ title: "Create new user" })
+  res.send({title: "Create new user"})
 });
 
 userRouter.put("/:id", (req, res) => {
-  res.send({ title: "Update user" })
+  res.send({title: "Update user"})
 });
 
 userRouter.delete("/:id", (req, res) => {
-  res.send({ title: "Delete user" })
+  res.send({title: "Delete user"})
 })
 
 export default userRouter;
